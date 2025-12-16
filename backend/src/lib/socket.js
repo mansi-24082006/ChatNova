f6228b7833c0c443d471ff76f59bd1d715e4699b
Mansi Vaghasiya <vaghasiyamansi999@gmail.com>
2025-12-16 12:08:34 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -14,7 +14,7 @@ export let io;
 export function initSocket(server) {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://chatnova-jsq3.onrender.com",
       credentials: true,
     },
   });
