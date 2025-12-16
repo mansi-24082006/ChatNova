d7ae16c2e76b3668dd29cf5c368365d14cc8578f
Mansi Vaghasiya <vaghasiyamansi999@gmail.com>
2025-12-16 12:09:35 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chatnova-jsq3.onrender.com",
     credentials: true,
   })
 );
