295ad6edbb80c788bb1fc8b5e1a608f2155721ac
Mansi Vaghasiya <vaghasiyamansi999@gmail.com>
2026-01-14 13:55:36 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.MODE === "development" ? "http://localhost:5002" : "/api",
+    import.meta.env.MODE === "development" ? "https://chatnova-backend-5hq3.onrender.com" : "/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
